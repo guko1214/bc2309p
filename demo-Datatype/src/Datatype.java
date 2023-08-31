5fda1bd1f02c2812b8b6cc64e5ea32c8ed2625a9
derrick.shum <guko_1214@hotmail.com>
2023-08-31 21:57:31 +0800

    Hello world

---
+++ demo-Datatype/src/Datatype.java
@@ -0,0 +1,14 @@
+public class Datatype {
+  public static void main(String[] args) {
+
+    // Assign value "Hello World!!!!" to a variable box
+    String box = "Hello World!!!";
+
+    System.out.println(box); // Print the value of a variable: Hello World!!!!
+    System.out.println("box"); // Print the string value: box
+
+    String str; // Declaration 聲明
+    str = "*&#bew$#"; // Assign value "abc" to a variable str
+    System.out.println(str); // &#bew$#
+  }
+}
---
+++ demo-Helloworld/src/Helloworld.java
@@ -0,0 +1,12 @@
+// this is the head of the file
+public class Helloworld {
+  // This is comment, I can type anything here
+  public static void main(String[] args) {
+    // Program starts ...
+    //  Tool: System.out.println
+    System.out.println("Hello World !!!! 你好！");
+    System.out.println("早上好嗎？");
+    System.out.println("I am fine.");
+  }
+}
+// This is the end of file
