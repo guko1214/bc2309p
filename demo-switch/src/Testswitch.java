a39215227a4ac422614f90555c4978f518ca79cc
derrick.shum <guko_1214@hotmail.com>
2023-09-05 23:18:06 +0800

    update switch

--- demo-switch/src/Testswitch.java
+++ demo-switch/src/Testswitch.java
@@ -4,8 +4,12 @@ public class Testswitch {
     char grade = ' ';
 
     // switch example 1
-    switch(score) {
-      case 90: // ~ for any case, if score >= case value will run, to apply for real use add break in end of each case, see example 2
+    switch (score) { 
+      //if the score value match one of the case, 
+      // unless a break is added in the matched case,
+      // it will continue the remaining cases following the sequence of case in the written code
+      // until a case with break or default case
+      case 90: 
         grade = 'A'; // 2, execute
         System.out.println("case 90");
         System.out.println(grade);
@@ -16,7 +20,7 @@ public class Testswitch {
         System.out.println(grade);
         // break;
       case 70:
-        grade = 'C'; 
+        grade = 'C';
         System.out.println("case 70");
         System.out.println(grade);
         // break;
@@ -30,10 +34,39 @@ public class Testswitch {
         System.out.println("default case");
         System.out.println(grade);
     }
-    System.out.println("The grade is " +grade);
+    System.out.println("The grade is " + grade);
+
+
+    switch (score) {
+      case 90:
+        grade = 'A'; // 2, execute
+        System.out.println("case 90");
+        System.out.println(grade);
+        // break;
+      case 70:
+        grade = 'C';
+        System.out.println("case 70");
+        System.out.println(grade);
+        // break;
+      case 80:
+        grade = 'B';
+        System.out.println("case 80");
+        System.out.println(grade);
+        // break;
+      case 60:
+        grade = 'D';
+        System.out.println("case 60");
+        System.out.println(grade);
+        // break;
+      default:
+        grade = 'F';
+        System.out.println("default case");
+        System.out.println(grade);
+    }
+    System.out.println("The grade is " + grade);
 
     // switch example 2
-    switch(score) {
+    switch (score) {
       case 90: // 1, match
         grade = 'A'; // 2, execute
         // System.out.println("case 90");
@@ -43,7 +76,7 @@ public class Testswitch {
         // System.out.println("case 80");
         break;
       case 70:
-        grade = 'C'; 
+        grade = 'C';
         // System.out.println("case 70");
         break;
       case 60:
@@ -54,7 +87,7 @@ public class Testswitch {
         grade = 'F';
         // System.out.println("default case");
     }
-    System.out.println("The grade is " +grade);
+    System.out.println("The grade is " + grade);
 
     // Alternative
     if (score >= 90) {
